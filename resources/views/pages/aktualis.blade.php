fffa8533cf0450c704028ab43303d7d5060ee38c
KassaiterAdmin <rendszergazda.kassaiter@gmail.com>
2017-12-05 12:54:28 +0100

    add new event

--- resources/views/pages/aktualis.blade.php
+++ resources/views/pages/aktualis.blade.php
@@ -5,8 +5,11 @@
         <div class="row color-bg">
             <div class="col-md-10 col-md-offset-1">
                 <h3>Aktuális programjaink</h3>
-                <img class="center" src="img/kka_sznap2017.jpg" alt="plakat"/>
-                <h4>Korábbi programjaink:</h4>
+                <img class="center plakat" src="img/nostra.jpg" alt="plakat"/>
+                <h4>Korábbi híreink:</h4>
+                <p>
+                    Születésnapi ünnepség - 2017 (<a href="img/kka_sznap2017.jpg">plakát megtekintése</a>)
+                </p>
                 <p>
                     Keresztény országértékelő - 2016 (<a href="img/orszagertekelo2016.png">plakát megtekintése</a>)
                 </p>
